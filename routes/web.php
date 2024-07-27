3ab9fef74bf6a43b6736e2016a698134418a6953
Abdelrahman Magdy EL Monshed <amgo123@gmail.com>
2024-07-27 22:51:25 +0300

    handle front end 2

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\backend\ProductController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.welcome');
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-// Route::get('/products/{id}/edit', [ProductController::class, 'edit']);
+
 require __DIR__.'/admin.php';
